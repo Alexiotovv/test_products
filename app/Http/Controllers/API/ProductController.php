c980167a4e636a4d6f7577c66267e3db4a400ce9
Alexiotovv <gpsalexvasquez@gmail.com>
2024-10-05 09:58:55 -0500

    test_alexvasquez

---
+++ app/Http/Controllers/API/ProductController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Validation\ValidationException;
+
+class ProductController extends Controller
+{
+    //
+    public function get(){
+        try { 
+            $data = Product::get();
+            return response()->json($data, 200);
+        } catch (\Throwable $th) {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    public function create(Request $request){
+        try {
+
+            $validated = $request->validate([
+                'name' => 'required|string|max:255',
+                'description' => 'required|string|max:255', 
+                'price' => 'required|numeric',
+                'stock' => 'required|integer|between:1,100000', 
+            ]);
+            
+
+            $data['name'] = $request['name'];
+            $data['description'] = $request['description'];
+            $data['price'] = $request['price'];
+            $data['stock'] = $request['stock'];
+            $res = Product::create($data);
+            return response()->json( $res, 200);
+        } catch (ValidationException $e) {
+            return response()->json(['status' => 'error', 'message' => 'Error de validación', 'errors' => $e->errors()], 422);
+        } catch (\Throwable $th) {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    public function getById($id){
+        try { 
+            $data = Product::find($id);
+            return response()->json($data, 200);
+        } catch (\Throwable $th) {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    public function update(Request $request,$id){
+        try { 
+            $data['name'] = $request['name'];
+            $data['description'] = $request['description'];
+            $data['price'] = $request['price'];
+            $data['stock'] = $request['stock'];
+
+            Product::find($id)->update($data);
+            $res = Product::find($id);
+            return response()->json( $res , 200);
+        } catch (\Throwable $th) {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+
+    public function delete($id){
+        try {       
+            $res = Product::find($id)->delete(); 
+            return response()->json([ "deleted" => $res ], 200);
+        } catch (\Throwable $th) {
+            return response()->json([ 'error' => $th->getMessage()], 500);
+        }
+    }
+}
